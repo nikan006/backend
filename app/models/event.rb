ff6956965ce4aca4cc5bfa56fb620f3355632d97
岸拓成 <88487269+nikan006@users.noreply.github.com>
2021-10-16 20:33:36 +0900

    アクションの設定

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  validates :name, presence:true, length:{maxmum:100}
+  validates :name, presence:true, length:{maximum:100}
   validates :start, presence:true
   validates :end, presence:true
 end
