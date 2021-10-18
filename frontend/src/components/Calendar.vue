b7e36d71edeb33996243c085c409c49b6f932060
岸拓成 <88487269+nikan006@users.noreply.github.com>
2021-10-18 19:04:57 +0900

    予定詳細記述

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -11,38 +11,53 @@
       <v-toolbar-title>{{ title }}</v-toolbar-title>
     </v-sheet>
     <v-sheet height="94vh">
-        <v-calendar
+      <v-calendar
         ref="calendar"
-          v-model="value"
-           :events="events"
-          @change="fetchEvents"
-          locale="ja-jp"
-          :day-format="(timestamp) => new Date(timestamp.date).getDate()"
-          :month-format="(timestamp) => (new Date(timestamp.date).getMonth() + 1) + ' /'"
-        ></v-calendar>
-      </v-sheet>
+        v-model="value"
+        :events="events"
+        @change="fetchEvents"
+        locale="ja-jp"
+        :day-format="(timestamp) => new Date(timestamp.date).getDate()"
+        :month-format="(timestamp) => (new Date(timestamp.date).getMonth() + 1) + ' /'"
+        @click:event="showEvent"
+      ></v-calendar>
+    </v-sheet>
+
+      <v-dialog :value="event !== null" @click:outside="closeDialog" width="600">
+        <EventDetailDialog v-if="event !== null" />
+      </v-dialog>
   </div>
 </template>
 
 <script>
 import { format } from 'date-fns';
 import { mapGetters, mapActions } from 'vuex';
+import EventDetailDialog from './EventDetailDialog';
 
 export default {
   name: 'Calendar',
+   components: {
+      EventDetailDialog,
+    },
   data: () => ({
     value: format(new Date(), 'yyyy/MM/dd'),
   }),
   computed: {
-    ...mapGetters('events', ['events']),
+    ...mapGetters('events', ['events','event']),
     title() {
       return format(new Date(this.value), 'yyyy年 M月');
     },
   },
   methods: {
-    ...mapActions('events', ['fetchEvents']),
+    ...mapActions('events', ['fetchEvents','setEvent']),
     setToday() {
       this.value = format(new Date(), 'yyyy/MM/dd')
+    },
+    showEvent({event}){
+      this.setEvent(event);
+    },
+    closeDialog() {
+      this.setEvent(null);
     },
   },
 };
