acaea61c93be89eabcbce4a96f48ccb035b93c5b
岸拓成 <88487269+nikan006@users.noreply.github.com>
2021-10-18 12:45:09 +0900

    カレンダー表示

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,11 +2,10 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import events from './modules/events';
 
-
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    events
-  }
+    events,
+  },
 });
